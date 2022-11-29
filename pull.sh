92fd0bd8ddc0c36042b717717dbfb7bb1b7ebf47
MxShun <16466322+MxShun@users.noreply.github.com>
2022-11-30 02:43:11 +0900

    update pull.sh

--- pull.sh
+++ pull.sh
@@ -4,11 +4,13 @@ set -eux
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
+git pull
+
 # シンボリックリンク
 filenames=`find . -mindepth 2 -type f -name ".*" -not -path ".git/*"`
-for filename in filenames; do
+for filename in $filenames; do
     ln -snfv ${PWD}/"$filename" ~/
 done
 
 # Vim colorscheme 導入
-#curl https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim > ~/.vim/colors/solarized.vim
+curl https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim > ~/.vim/colors/solarized.vim
